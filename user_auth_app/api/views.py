016d178fac4330508302e35d1835200ab01d97da
Nils Boelingen <54119982+NilsBoelingen@users.noreply.github.com>
2024-11-05 22:30:33 +0100

    auth with email and password

--- user_auth_app/api/views.py
+++ user_auth_app/api/views.py
@@ -5,6 +5,8 @@ from rest_framework.permissions import AllowAny
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.views import APIView
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 from user_auth_app.models import UserProfile
 from .serializer import UserProfileSerializer, RegistrationSerializer
 
@@ -20,21 +22,41 @@ class CustomLoginView(ObtainAuthToken):
     permission_classes = [AllowAny]
 
     def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-        data = {}
+        email = request.data.get('email')
+        password = request.data.get('password')
 
-        if serializer.is_valid():
-            user = serializer.validated_data['user']
-            token, created = Token.objects.get_or_create(user=user)
-            data = {
-                'token': token.key,
-                'username': user.username,
-                'email': user.email
-            }
-            return Response(data, status=status.HTTP_202_ACCEPTED)
-        else:
-            data = serializer.errors
-            return Response(data, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        user = authenticate(username=user.username, password=password)
+        if user is None:
+            return Response({'error': 'Invalid email or password'}, status=status.HTTP_401_UNAUTHORIZED)
+        
+        token, created = Token.objects.get_or_create(user=user)
+        data = {
+            'token': token.key,
+            'username': user.username,
+            'email': user.email
+        }
+        return Response(data, status=status.HTTP_202_ACCEPTED)
+
+        # serializer = self.serializer_class(data=request.data)
+        # data = {}
+
+        # if serializer.is_valid():
+        #     user = serializer.validated_data['user']
+        #     token, created = Token.objects.get_or_create(user=user)
+        #     data = {
+        #         'token': token.key,
+        #         'username': user.username,
+        #         'email': user.email
+        #     }
+        #     return Response(data, status=status.HTTP_202_ACCEPTED)
+        # else:
+        #     data = serializer.errors
+        #     return Response(data, status=status.HTTP_400_BAD_REQUEST)
 
 class RegistrationView(APIView):
     permission_classes = [AllowAny]
